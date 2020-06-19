48fea83184b27e0cbd1679fed373d4d48cf26416
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-06-19 22:58:22 +0300

    Create homePage.js

---
+++ my-app/UI/src/homePage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+function HomePage(){
+    return(
+<div className="Logo">
+      <header className="App-header">
+        <img src="./checkBox.jpg" className="App-logo" alt="logo" />
+        <p>If you have a tendency to forget what are your tasks this app is perfect for you !<br></br>
+          Welcome to ToDO :) <br></br> An app where you can write each of your tasks.
+        </p>
+        <div className="signIn">
+      <button class="button"><Link to="/SignIn">Sign In</Link></button>
+    </div>
+    <p>Or if you haven't registered yet:</p>
+    <div className="signUp">
+      <button class="button"><Link to="/SignUp">Sign Up</Link></button>
+          </div>
+          </header>
+    </div>
+    )
+}
+    export default HomePage;
