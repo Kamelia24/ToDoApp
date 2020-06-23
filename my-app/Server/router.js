f7766a28106dcfd77de44c14ee3b41e5abd91339
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-06-23 14:33:02 +0300

    Create router.js

---
+++ my-app/Server/router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const controller=require('./controller.js');
+
+router.get('/checkUser',controller.checkUser); 
+router.get('/addUser',controller.addUser); 
+router.get('/addTask',controller.addTask); 
+router.get('/getTasks',controller.getTasks); 
+//router.get('/',controller.);
+module.exports = router;
