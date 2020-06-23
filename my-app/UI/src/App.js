9ab240245ea10d9eff0b0f6d28ccc427041f1c59
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-06-23 14:33:45 +0300

    Update App.js

--- my-app/UI/src/App.js
+++ my-app/UI/src/App.js
@@ -5,6 +5,8 @@ import { Route,Switch} from "react-router-dom";
 import HomePage from "./homePage"
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
+import ToDoList from "./showTasks";
+import addTask from "./addTask";
 //import HomePage from "./index";
 function Home() {
   return (
@@ -13,23 +15,12 @@ function Home() {
         <Route exact path="/" component={HomePage} />
         <Route path="/SignIn" component={SignIn} />
         <Route path="/SignUp" component={SignUp} />
-    </Switch>
+        <Route path="/ToDoList" component={ToDoList}/>
+        <Route path="/AddNewTask" component={addTask} />
+        </Switch>
   );
 }
-/*function SignIn(){
-  return(
-    <div className="signIn">
-      <button class="button" onClick={() => history.push('/SignIn')}>Sign In</button>
-    </div>
-  )
-}
-function SignUp(){
-  return(
-    <div className="signUp">
-      <button class="button" onClick={() => history.push('/SignUp')}>Sign Up</button>
-    </div>
-  );
-}*/
+
 
 
 
