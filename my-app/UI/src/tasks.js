dd2b848f97c491af63a52c2c602e4247ca09bab2
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-06-25 14:05:12 +0300

    Update tasks.js

--- my-app/UI/src/tasks.js
+++ my-app/UI/src/tasks.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route,Switch} from "react-router-dom";
-let Tasks=[
+import axios from 'axios';
+//import ReactDOM from 'react-dom';
+//import { Route,Switch} from "react-router-dom";
+
+async function addTasks(){
+    let Tasks;
+    try{
+let res =await axios.get('http://localhost:4000/getTasks')
+            /*.then((res) => {
+                console.log("res",res)
+                
+                
+            }).catch((error) => {
+                console.log("errorrrr:",error)
+            });*/
+            Tasks=res.data["info"];
+            console.log(Tasks)
+/*let Tasks=[
     {title:"cleaning",description:"clean the room",dateCreated:"17.06.20",deadline:"19.06.20",status:"active"},
     {title:"trash",description:"take out the trash",dateCreated:"18.06.20",deadline:"20.06.20",status:"active"}];
-let output=Tasks.map((Tasks)=> <tr>
-    <td>{Tasks.title}</td>
-    <td>{Tasks.description}</td>
-    <td>{Tasks.dateCreated}</td>
-    <td>{Tasks.deadline}</td>
-    <td><input type="checkbox" class="checkmark"></input></td>
-    </tr>);
-function addTasks(){
-     
+*/
+let output=Tasks.map((Task)=> <tr>
+    <td >{Task.title}</td>
+    
+    <td >{Task.description}</td>
+    
+    <td >{Task.dateCreated}</td>
+    
+    <td >{Task.deadline}</td>
+    
+    <td ><input type="checkbox" className="checkmark"></input></td>
+    
+    </tr>); 
     return(
        
         output
     )
+    }catch(err){
+        console.log("Error!")
+    }
 
 }
 
