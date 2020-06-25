5ee5d55fe7a5dd329863c8989bf77a5b3d3b606c
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-06-25 14:05:04 +0300

    Update SignIn.js

--- my-app/UI/src/SignIn.js
+++ my-app/UI/src/SignIn.js
@@ -1,12 +1,12 @@
 import React from "react";
-import axios from 'axios';
+//import axios from 'axios';
 function SignInForm(){
         return (
             <form >
-            <label for="username">Username:</label>
+            <label htmlFor="username">Username:</label>
             <input type="text" id="username" name="username" placeholder="Username" />
   <br></br>
-            <label for="pass">Password:</label>
+            <label htmlFor="pass">Password:</label>
             <input type="password" id="pass" name="pass" placeholder="Password" />
   <br></br>
       
