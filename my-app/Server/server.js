40599d0bc896b1ee05fbcdfc9e07b77ac55ea79a
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-06-29 15:21:38 +0300

    Update server.js

--- my-app/Server/server.js
+++ my-app/Server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const Router = require('./router.js');
 var cors = require('cors');
 app.use(cors());
-app.all('/addUser', function(req, res, next) {
+/*app.all('/addUser', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
@@ -27,8 +27,9 @@ app.all('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
-});
+});*/
 app.use(express.static(__dirname + '/public'));
+app.use(express.json())
 app.use(Router);
-let port=4000;
+let port=5000;
 app.listen(port,()=>{console.log(`Node JS API is listening on port: ${port}`);});
