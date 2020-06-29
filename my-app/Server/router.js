c9e1b8b2fe5b415d6d08a3b4ea8ce9cd3f04ec70
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-06-29 15:21:26 +0300

    Update router.js

--- my-app/Server/router.js
+++ my-app/Server/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller=require('./controller.js');
 
-router.get('/checkUser',controller.checkUser); 
+router.post('/checkUser',controller.checkUser); 
 router.post('/addUser',controller.addUser); 
 router.get('/addTask',controller.addTask); 
 router.get('/getTasks',controller.getTasks); 
