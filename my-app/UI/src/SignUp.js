22efd62179fab459134346b7b539ea6ba83700aa
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-03 14:16:13 +0300

    Update SignUp.js

--- my-app/UI/src/SignUp.js
+++ my-app/UI/src/SignUp.js
@@ -1,10 +1,11 @@
-import React,{Component}  from "react";
+import React,{useState}  from "react";
+import {Link,useHistory} from 'react-router-dom';
 //import { render } from "@testing-library/react";
 import axios from 'axios';
 
-class SignUpForm extends Component{
-
-    constructor(props) {
+const SignUpForm=()=>{
+const history=useHistory();
+    /*constructor(props) {
         super(props)
 
         this.onChangeUserName = this.onChangeUserName.bind(this);
@@ -33,47 +34,66 @@ class SignUpForm extends Component{
         this.setState({ password: e.target.value })
     }
     onSubmit(e) {
-        e.preventDefault()
+        e.preventDefault()*/
         /*const params = new URLSearchParams();
         params.append('name', this.state.name);
         params.append('username', this.state.username);
         params.append('password', this.state.password);
         params.append('age',this.state.age)*/
-        const userObject = {
+        /*const userObject = {
             name: this.state.name,
             username: this.state.username,
             password: this.state.password,
             age: this.state.age
         };
 console.log(userObject)
-        /*axios.post('/addUser', userObject)
+        axios.post('http://localhost:5000/addUser', userObject)
             .then((res) => {
                 console.log("in Axios",userObject)
+                history.push('/SignIn')
             }).catch((error) => {
                 console.log(error)
-            });*/
-            axios({method:'post',url:'http://localhost:5000/addUser',data:userObject});
+            });
+            //axios({method:'post',url:'http://localhost:5000/addUser',data:userObject});
             console.log(userObject)
         this.setState({ name: '', age: '' ,username: '',password: ''})
+    }*/
+    const [name,setName]=useState("");
+    const [age,setAge]=useState("");
+    const [password,setPassword]=useState("");
+    const [username,setUsername]=useState("");
+    const Post=()=>{
+        const userObject = {
+            name:name,
+            username:username,
+            password:password,
+            age:age
+        };
+        axios.post('http://localhost:5000/addUser', userObject)
+            .then((res) => {
+                console.log("in Axios",userObject,res)
+                history.push('/SignIn')
+            }).catch((error) => {
+                console.log(error)
+            });
     }
-render(){
     return (
-      <form  onSubmit={this.onSubmit}>
+      <form>
       <label htmlFor="username">Username:</label>
-      <input type="text" id="username" value={this.state.username} name="username" onChange={this.onChangeUserUsername} placeholder="Username" />
+      <input type="text" id="username" value={username} name="username" onChange={(e)=>setUsername(e.target.value)} placeholder="Username" />
   <br></br>
       <label htmlFor="pass">Password:</label>
-      <input type="password" id="pass" name="pass" value={this.state.password} onChange={this.onChangeUserPassword} placeholder="Password" />
+      <input type="password" id="pass" name="pass" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password" />
   <br></br>
       <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name" value={this.state.name} onChange={this.onChangeUserName} placeholder="First name" />
+      <input type="text" id="name" name="name" value={name} onChange={(e)=>setName(e.target.value)} placeholder="First name" />
       <br></br>
       <label htmlFor="age">Age:</label>
-      <input type="number" id="age" name="age" value={this.state.age} onChange={this.onChangeUserAge} placeholder="Age" />
+      <input type="number" id="age" name="age" value={age} onChange={(e)=>setAge(e.target.value)} placeholder="Age" />
   <br></br>
-      <input type="submit" value="Submit"/>
+      <input type="submit" value="Submit" onClick={()=>Post()}/>
     </form>
     );
-}
+
   }
   export default SignUpForm;
