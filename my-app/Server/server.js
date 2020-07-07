e2dd32e3bc14b571555ad33806962624c0b4c5c9
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-07 14:13:01 +0300

    Update server.js

--- my-app/Server/server.js
+++ my-app/Server/server.js
@@ -3,31 +3,6 @@ const app = express();
 const Router = require('./router.js');
 var cors = require('cors');
 app.use(cors());
-/*app.all('/addUser', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
-app.all('/checkUser', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
-app.all('/addTask', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
-app.all('/getTasks', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});*/
 app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 app.use(Router);
