02efc1ecc88472f4c8e6ca9b684381ae4216fc03
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-07 09:26:29 +0300

    Update homePage.js

--- my-app/UI/src/homePage.js
+++ my-app/UI/src/homePage.js
@@ -9,11 +9,11 @@ function HomePage(){
           Welcome to ToDO :) <br></br> An app where you can write each of your tasks.
         </p>
         <div className="signIn">
-      <button className="button"><Link to="/SignIn">Sign In</Link></button>
+      <button className="button"><Link to="/LogIn">Sign In</Link></button>
     </div>
     <p>Or if you haven't registered yet:</p>
     <div className="signUp">
-      <button className="button"><Link to="/SignUp">Sign Up</Link></button>
+      <button className="button"><Link to="/Register">Sign Up</Link></button>
       <button className="button"><Link to="/toDoList">Tasks</Link></button>
           </div>
           </header>
