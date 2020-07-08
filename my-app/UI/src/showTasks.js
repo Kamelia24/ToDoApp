1ef95de6674d3d670e492d18198e7f2cb07f7713
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-08 14:56:11 +0300

    Update showTasks.js

--- my-app/UI/src/showTasks.js
+++ my-app/UI/src/showTasks.js
@@ -3,28 +3,26 @@ import Tasks from "./tasks";
 import { Link} from "react-router-dom";
 function showTask(){
     return(
-        
-            <div id="taskContainer">
-        <div>
-            <div className="button1" ><Link to="/AddNewTask">New task</Link></div>
-        
+        <div id="taskContainer">
+            <div>
+                <div className="button1" ><Link to="/AddNewTask">New task</Link></div>
+            </div>
+            <h1 align="center">Your active tasks</h1>
+            <table id="todo">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Date Created</th>
+                        <th>Deadline</th>
+                        <th>Finished</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <Tasks />
+                </tbody>
+            </table>
         </div>
-    <h1 align="center">Your active tasks</h1>
-    <table id="todo">
-        <thead>
-        <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Date Created</th>
-            <th>Deadline</th>
-            <th>Finished</th>
-        </tr>
-        </thead>
-        <tbody>
-        <Tasks />
-        </tbody>
-    </table>
-    </div>
     )
 }
 export default showTask;
