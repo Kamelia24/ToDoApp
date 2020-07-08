0bb4cb88c9ef3dff9dc9e9ce0580c243329da9bf
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-08 14:56:00 +0300

    Update addTask.js

--- my-app/UI/src/addTask.js
+++ my-app/UI/src/addTask.js
@@ -23,41 +23,41 @@ const addTask =()=>{
                 console.log(error)
             });
     }
- return(
-     <div id="addTask">
-         <h1>Add new task:</h1>
-    <form  onSubmit={(e)=>Post(e)}>
-    <label for="title">Title*:</label>
-    <input type="text" id="title"
-    value={title}
-    onChange={(e)=>setTitle(e.target.value)}
-    name="title"
-    placeholder="title" required/>
-<br></br>
-    <label for="description">Description:</label>
-    <input type="text" id="description"
-    value={description}
-    onChange={(e)=>setDescription(e.target.value)}
-    name="description" 
-    placeholder="description" />
-<br></br>
-    <label for="location">Location:</label>
-    <input type="text" id="location"
-    value={location}
-    onChange={(e)=>setLocation(e.target.value)}
-    name="location"
-    placeholder="location" />
-    <br></br>
-    <label for="deadline">Deadline:</label>
-    <input type="number" id="deadline"
-    value={deadline}
-    onChange={(e)=>setDeadline(e.target.value)}
-    name="deadline"
-    placeholder="deadline" />
-<br></br>
-    <input type="submit" value="Add task"/>
-  </form>
-  </div>
- );
+    return(
+        <div id="addTask">
+            <h1>Add new task:</h1>
+            <form  onSubmit={(e)=>Post(e)}>
+                <label for="title">Title*:</label>
+                <input type="text" id="title"
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}
+                name="title"
+                placeholder="title" required/>
+                <br></br>
+                <label for="description">Description:</label>
+                <input type="text" id="description"
+                value={description}
+                onChange={(e)=>setDescription(e.target.value)}
+                name="description" 
+                placeholder="description" />
+                <br></br>
+                <label for="location">Location:</label>
+                <input type="text" id="location"
+                value={location}
+                onChange={(e)=>setLocation(e.target.value)}
+                name="location"
+                placeholder="location" />
+                <br></br>
+                <label for="deadline">Deadline:</label>
+                <input type="number" id="deadline"
+                value={deadline}
+                onChange={(e)=>setDeadline(e.target.value)}
+                name="deadline"
+                placeholder="deadline" />
+                <br></br>
+                <input type="submit" value="Add task"/>
+            </form>
+        </div>
+    );
 }
 export default addTask;
