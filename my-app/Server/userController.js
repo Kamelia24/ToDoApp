9421affcf068aab48193760713e8d76f3f249d24
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-08 14:55:55 +0300

    Update userController.js

--- my-app/Server/userController.js
+++ my-app/Server/userController.js
@@ -41,8 +41,9 @@ module.exports={
             const token = jwt.sign({id:userID},JWT_SECRET);
             result.json({token:token,userID:userID})
             console.log(token)
-        }else{outp="Incorect username or password,please try again!";
-        result.send(outp);
+        }else{
+            outp="Incorect username or password,please try again!";
+            result.send(outp);
         }
     },
     addUser:async function(req,result){
@@ -56,9 +57,7 @@ module.exports={
             res=await client.query(`SELECT * FROM public.users WHERE username='${username}'`)
             if(res.rows[0]===undefined){
                 hasUserIs=body;
-
                 console.log("no users here")
-                
             }else{
                 result.send('User already exists')
             }
@@ -73,7 +72,9 @@ module.exports={
             let name=body.name;
             let hashedPassword;
             console.log(username,password,age,name)
-            if(age<10 || age >90){result.send("Age is not valid!");}
+            if(age<10 || age >90){
+                result.send("Age is not valid!");
+            }
             try{
                 res=await bcrypt.hash(password, saltRounds)
                 hashedPassword = res;
@@ -86,13 +87,12 @@ module.exports={
                 res=await client.query(`insert into public.users
                 (name,username,password,age)
                 values('${name}','${username}','${hashedPassword}',${age})`);
-            console.log("inserting the data")
-            result.json('success');
+                console.log("inserting the data")
+                result.json('success');
             }catch(err) {
                 console.log ('insert user info:',err);
                 result.json(err)
             }
         }
-        
     }
 }
