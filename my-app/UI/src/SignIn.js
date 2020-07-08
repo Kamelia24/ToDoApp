b1c65240985cd2c88a3892341340901fd0b29d4d
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-08 13:03:32 +0300

    Update SignIn.js

--- my-app/UI/src/SignIn.js
+++ my-app/UI/src/SignIn.js
@@ -3,46 +3,6 @@ import axios from 'axios';
 import {Link,useHistory} from 'react-router-dom';
 const SignInForm =()=>{
     const history=useHistory();
-    /*constructor(props) {
-        super(props)
-        this.onChangeUserUsername = this.onChangeUserUsername.bind(this);
-        this.onChangeUserPassword = this.onChangeUserPassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            username:'',
-            password:''
-        }
-    }
-    onChangeUserUsername(e) {
-        this.setState({ username: e.target.value })
-    }
-    onChangeUserPassword(e) {
-        this.setState({ password: e.target.value })
-    }
-    onSubmit(e) {
-        e.preventDefault()
-        /*const params = new URLSearchParams();
-        params.append('name', this.state.name);
-        params.append('username', this.state.username);
-        params.append('password', this.state.password);
-        params.append('age',this.state.age)*/
-        /*const userObject = {
-            username: this.state.username,
-            password: this.state.password
-        };
-console.log(userObject)
-        axios.post('/checkUser', userObject)
-            .then((res) => {
-                console.log(res)
-            }).catch((error) => {
-                console.log(error)
-            });
-            /*axios({method:'post',url:'http://localhost:5000/checkUser',data:userObject});*/
-            /*console.log(userObject)
-        this.setState({ name: '', age: '' ,username: '',password: ''})
-    }*/
-    
     const [password,setPassword]=useState("");
     const [username,setUsername]=useState("");
     const Post=(e)=>{
