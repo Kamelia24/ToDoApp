816d47ccd0658b29e8884c9af19527fee2108f82
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-08 14:56:04 +0300

    Update App.js

--- my-app/UI/src/App.js
+++ my-app/UI/src/App.js
@@ -6,19 +6,18 @@ import HomePage from "./homePage"
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import ToDoList from "./showTasks";
-import addTask from "./addTask";
+//import addTask from "./addTask";
 //import HomePage from "./index";
 function Home() {
-  return (
-    
+    return (
         <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/LogIn" component={SignIn} />
         <Route path="/Register" component={SignUp} />
         <Route path="/ToDoList" component={ToDoList}/>
-        <Route path="/AddNewTask" component={addTask} />
+        
         </Switch>
-  );
+    );
 }
 
 
