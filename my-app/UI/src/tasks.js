a3a215ff5bc78e7d853c1928fc9a2a300e9dfb4d
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-08 14:56:23 +0300

    Update tasks.js

--- my-app/UI/src/tasks.js
+++ my-app/UI/src/tasks.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
-class Task extends React.Component{
-    state = {
+const Task =()=>{
+    /*state = {
         isLoading: true,
         Tasks: [],
         errors: null
-      }
-      componentDidMount(){
-    let Tasks;
-    console.log(localStorage.getItem("jwt"))
-    axios.get('http://localhost:5000/getTasks',{
-        headers: {
-        'Authorization':"Bearer "+localStorage.getItem("jwt")
-        }
-    })
-            .then((res) => {
-                console.log("res",res)
-                Tasks=res.data["info"];
-                this.setState({
-                    Tasks,
-                    isLoading:false
-                })
-                 console.log(Tasks)
-            })/*.then(Tasks=>{
-                
-                console.log(this.state)
+    }*/
+    let [isLoading,setIsLoading]=useState("false");
+    let [Tasks,setTasks]=useState("");
+    useEffect(() => {
+        let Tasks;
+        console.log(localStorage.getItem("jwt"))
+        axios.get('http://localhost:5000/getTasks',{
+            headers: {
+            'Authorization':"Bearer "+localStorage.getItem("jwt")
             }
-            )*/.catch((error) => {
-                console.log("errorrrr:",error)
-                this.setState({error,isLoading:false})
-            });
-            
-           
-        }
+        })
+        .then((res) => {
+            console.log("res",res)
+            Tasks=res.data["info"];
+            /*this.setState({
+                Tasks,
+                isLoading:false
+            })*/
+            setIsLoading("false");
+            setTasks(Tasks);
+            console.log(Tasks,isLoading)
+        })
+        .catch((error) => {
+            console.log("errorrrr:",error)
+            //this.setState({error,isLoading:false})
+            setIsLoading("false");
+            console.log(isLoading);
+        });
+    }, [])
 /*let Tasks=[
     {title:"cleaning",description:"clean the room",dateCreated:"17.06.20",deadline:"19.06.20",status:"active"},
     {title:"trash",description:"take out the trash",dateCreated:"18.06.20",deadline:"20.06.20",status:"active"}];
@@ -50,32 +51,33 @@ class Task extends React.Component{
     
     </tr>);*/
     
-    render() {
-        const { Tasks ,isLoading } = this.state;
-        console.log(this.state)
-    return(
-        <React.Fragment>
-        {!isLoading ? (
-          Tasks.map((Task)=> <tr>
-          <td >{Task.title}</td>
+ 
+        //const { Tasks ,isLoading } = this.state;
+        //console.log(this.state)
+        return (
+            
+            <React.Fragment>
+                {true ? (
+                Tasks.map((Task)=> <tr>
+                    <td >{Task.title}</td>
           
-          <td >{Task.description}</td>
+                    <td >{Task.description}</td>
           
-          <td >{Task.dateCreated}</td>
+                    <td >{Task.dateCreated}</td>
           
-          <td >{Task.deadline}</td>
+                    <td >{Task.deadline}</td>
           
-          <td ><input type="checkbox" className="checkmark"></input></td>
+                    <td ><input type="checkbox" className="checkmark"></input></td>
           
-          </tr>)
-        ) : (
+                </tr>)
+                ) : (
             <tr>
-          <td>Loading...</td>
-          </tr>
-        )}
+                <td>Loading...</td>
+            </tr>
+            )}
         
         </React.Fragment>
-    )
-        }
-    }
+        )
+    
+}
 export default Task;
