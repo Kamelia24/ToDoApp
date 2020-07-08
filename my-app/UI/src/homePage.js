fb6118ccc76740f95f6879ff5e4403a0ab087318
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-08 14:56:07 +0300

    Update homePage.js

--- my-app/UI/src/homePage.js
+++ my-app/UI/src/homePage.js
@@ -2,22 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function HomePage(){
     return(
-<div className="Logo">
-      <header className="App-header">
-        <img src="./checkBox.jpg" className="App-logo" alt="logo" />
-        <p>If you have a tendency to forget what are your tasks this app is perfect for you !<br></br>
+      <div className="Logo">
+        <header className="App-header">
+          <img src="./checkBox.jpg" className="App-logo" alt="logo" />
+          <p>If you have a tendency to forget what are your tasks this app is perfect for you !<br></br>
           Welcome to ToDO :) <br></br> An app where you can write each of your tasks.
-        </p>
-        <div className="signIn">
-      <button className="button"><Link to="/LogIn">Sign In</Link></button>
-    </div>
-    <p>Or if you haven't registered yet:</p>
-    <div className="signUp">
-      <button className="button"><Link to="/Register">Sign Up</Link></button>
-      <button className="button"><Link to="/toDoList">Tasks</Link></button>
+          </p>
+          <div className="signIn">
+            <button className="button"><Link to="/LogIn">Sign In</Link></button>
           </div>
-          </header>
-    </div>
+          <p>Or if you haven't registered yet:</p>
+          <div className="signUp">
+            <button className="button"><Link to="/Register">Sign Up</Link></button>
+            <button className="button"><Link to="/toDoList">Tasks</Link></button>
+          </div>
+        </header>
+      </div>
     )
 }
-    export default HomePage;
+export default HomePage;
