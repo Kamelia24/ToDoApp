b696d78e3f88fabd8be5a18d0c5c99fb00f94b72
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-08 14:56:15 +0300

    Update SignIn.js

--- my-app/UI/src/SignIn.js
+++ my-app/UI/src/SignIn.js
@@ -1,38 +1,49 @@
 import React,{useState} from "react";
 import axios from 'axios';
 import {Link,useHistory} from 'react-router-dom';
-const SignInForm =()=>{
+const SignInForm = () => {
     const history=useHistory();
     const [password,setPassword]=useState("");
     const [username,setUsername]=useState("");
-    const Post=(e)=>{
+    const signInRequest=(e)=>{
         e.preventDefault();
         const userObject = {
             username:username,
             password:password
         };
         axios.post('/checkUser', userObject)
-            .then((res) => {
-                //localStorage.setItem("jwt",res.token)
-                //localStorage.setItem("user",JSON.stringify(res.user))
-                console.log(res)
-                localStorage.setItem("jwt",res.data.token)
-                localStorage.setItem("userID",res.data.userID)
-                //console.log(localStorage.getItem("jwt"))
-                history.push('/ToDoList')
-            }).catch((error) => {
-                console.log(error)
-            });
+        .then((res) => {
+            //localStorage.setItem("jwt",res.token)
+            //localStorage.setItem("user",JSON.stringify(res.user))
+            console.log(res)
+            localStorage.setItem("jwt",res.data.token)
+            localStorage.setItem("userID",res.data.userID)
+            //console.log(localStorage.getItem("jwt"))
+            history.push('/ToDoList')
+        }).catch((error) => {
+            console.log(error)
+        });
     }
     return (
         <form>
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" placeholder="Username"  value={username} onChange={(e)=>setUsername(e.target.value)} />
+            <input type="text" id="username"
+            name="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)} />
+
             <br></br>
+
             <label htmlFor="pass">Password:</label>
-            <input type="password" id="pass" name="pass" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+            <input type="password" id="pass"
+            name="pass"
+            placeholder="Password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)} />
             <br></br>
-            <input type="submit" value="Submit"  onClick={(e)=>Post(e)}/>
+
+            <input type="submit" value="Submit"  onClick={(e)=>signInRequest(e)}/>
         </form>
     );       
 }
