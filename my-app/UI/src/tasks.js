8f7c4c4874642a4e82acccbf288eac820b1aa851
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-10 13:04:22 +0300

    Update tasks.js

--- my-app/UI/src/tasks.js
+++ my-app/UI/src/tasks.js
@@ -1,16 +1,11 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import M from 'materialize-css';
+import { Link} from "react-router-dom";
 const Task =()=>{
-    /*state = {
-        isLoading: true,
-        Tasks: [],
-        errors: null
-    }*/
     let [isLoading,setIsLoading]=useState(true);
     let [tasks,setTasks]=useState([]);
     useEffect(() => {
-        //let Tasks;
         console.log(localStorage.getItem("jwt"))
         axios.get('http://localhost:5000/getTasks',{
             headers: {
@@ -19,43 +14,17 @@ const Task =()=>{
         })
         .then((res) => {
             console.log("res",res)
-            //tasks=;
-            /*this.setState({
-                Tasks,
-                isLoading:false
-            })*/
             setIsLoading(false);
             setTasks(res.data["info"]);
             console.log(tasks,isLoading)
         })
         .catch((error) => {
             console.log("errorrrr:",error)
-            //this.setState({error,isLoading:false})
             M.toast({html: error})
             setIsLoading(false);
             console.log(isLoading);
         });
     }, [])
-/*let Tasks=[
-    {title:"cleaning",description:"clean the room",dateCreated:"17.06.20",deadline:"19.06.20",status:"active"},
-    {title:"trash",description:"take out the trash",dateCreated:"18.06.20",deadline:"20.06.20",status:"active"}];
-*/
-/*let output=Tasks.map((Task)=> <tr>
-    <td >{Task.title}</td>
-    
-    <td >{Task.description}</td>
-    
-    <td >{Task.dateCreated}</td>
-    
-    <td >{Task.deadline}</td>
-    
-    <td ><input type="checkbox" className="checkmark"></input></td>
-    
-    </tr>);*/
-    
- 
-        //const { Tasks ,isLoading } = this.state;
-        //console.log(this.state)
         return (
             
             
@@ -74,6 +43,7 @@ const Task =()=>{
                 </tr>)
                 ) : (
             <tr key={1}> 
+            
                 <td >Loading...</td>
             </tr>
             )
