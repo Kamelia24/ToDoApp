300ff4697fb4c5a22ca68d7b769dc9f7aefae0b5
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-10 13:04:14 +0300

    Update SignUp.js

--- my-app/UI/src/SignUp.js
+++ my-app/UI/src/SignUp.js
@@ -1,63 +1,9 @@
 import React,{useState}  from "react";
 import {useHistory} from 'react-router-dom';
-//import { render } from "@testing-library/react";
 import axios from 'axios';
 import M from 'materialize-css'
 const SignUpForm=()=>{
     const history=useHistory();
-    /*constructor(props) {
-        super(props)
-
-        this.onChangeUserName = this.onChangeUserName.bind(this);
-        this.onChangeUserAge = this.onChangeUserAge.bind(this);
-        this.onChangeUserUsername = this.onChangeUserUsername.bind(this);
-        this.onChangeUserPassword = this.onChangeUserPassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            name: '',
-            age: '',
-            username:'',
-            password:''
-        }
-    }
-    onChangeUserName(e) {
-        this.setState({ name: e.target.value })
-    }
-    onChangeUserUsername(e) {
-        this.setState({ username: e.target.value })
-    }
-    onChangeUserAge(e) {
-        this.setState({ age: e.target.value })
-    }
-    onChangeUserPassword(e) {
-        this.setState({ password: e.target.value })
-    }
-    onSubmit(e) {
-        e.preventDefault()*/
-        /*const params = new URLSearchParams();
-        params.append('name', this.state.name);
-        params.append('username', this.state.username);
-        params.append('password', this.state.password);
-        params.append('age',this.state.age)*/
-        /*const userObject = {
-            name: this.state.name,
-            username: this.state.username,
-            password: this.state.password,
-            age: this.state.age
-        };
-console.log(userObject)
-        axios.post('http://localhost:5000/addUser', userObject)
-            .then((res) => {
-                console.log("in Axios",userObject)
-                history.push('/SignIn')
-            }).catch((error) => {
-                console.log(error)
-            });
-            //axios({method:'post',url:'http://localhost:5000/addUser',data:userObject});
-            console.log(userObject)
-        this.setState({ name: '', age: '' ,username: '',password: ''})
-    }*/
     const [name,setName]=useState("");
     const [age,setAge]=useState("");
     const [password,setPassword]=useState("");
