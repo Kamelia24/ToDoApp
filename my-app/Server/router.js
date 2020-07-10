b812eb6ef235abe8d0811e8de76014a82989d7d6
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-10 13:03:28 +0300

    Update router.js

--- my-app/Server/router.js
+++ my-app/Server/router.js
@@ -4,9 +4,6 @@ const controller=require('./controller.js');
 const userController=require('./userController.js');
 const requireLogin = require('./requireLogin')
 router.post('/checkUser',userController.checkUser); 
-/*router.get('/protected',requireLogin, (req,res)=>{
-    res.send("hello user")
-});*/
 router.post('/addUser',userController.addUser); 
 router.post('/addTask',requireLogin,controller.addTask); 
 router.get('/getTasks',requireLogin,controller.getTasks); 
