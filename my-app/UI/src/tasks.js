d4c4bcfcda09a354ad956ead97bddf71cff3ee2e
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-13 14:38:42 +0300

    Update tasks.js

--- my-app/UI/src/tasks.js
+++ my-app/UI/src/tasks.js
@@ -1,8 +1,9 @@
 import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import M from 'materialize-css';
-import { Link} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 const Task =()=>{
+    const history=useHistory();
     let [isLoading,setIsLoading]=useState(true);
     let [tasks,setTasks]=useState([]);
     useEffect(() => {
@@ -20,25 +21,47 @@ const Task =()=>{
         })
         .catch((error) => {
             console.log("errorrrr:",error)
-            M.toast({html: error})
+            M.toast({html: "I think you haven't logged in"})
+            history.push("/");
             setIsLoading(false);
             console.log(isLoading);
         });
     }, [])
+    function removeTask(task){
+        console.log(task)
+        axios.post('http://localhost:5000/removeTask',{id:task.id},{
+            headers: {
+            'Authorization':"Bearer "+localStorage.getItem("jwt")
+            }
+        })
+        .then((res)=>{
+            console.log(res);
+            M.toast({html:"Successfully removed task"});
+            history.push('/ToDoList')
+        }).catch((err)=>{
+            console.log(err);
+            M.toast({html:"Error removing task"});
+        })
+    }
         return (
             
             
                 !isLoading ? (
                 tasks.map((task)=> <tr key={task.id+2}>
+                    
                     <td >{task.title}</td>
           
                     <td  >{task.description}</td>
           
-                    <td  >{task.dateCreated}</td>
+                    <td  >{task.date_created}</td>
+                    {task.status == "coming" ? (
+                        <td  style={{ color: 'red' }}>{task.deadline}</td>
+                    ):(
+                        <td  >{task.deadline}</td>
+                    )}
+                    
           
-                    <td  >{task.deadline}</td>
-          
-                    <td ><input type="checkbox" className="checkmark"></input></td>
+                    <td ><input type="checkbox" className="checkmark" onClick={removeTask(task)}></input></td>
           
                 </tr>)
                 ) : (
