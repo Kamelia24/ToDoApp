94428ae94f9fe319f22716d61f4da81c36e6e0b6
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-14 14:34:27 +0300

    Update App.js

--- my-app/UI/src/App.js
+++ my-app/UI/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import './App.css';
-import { Route,Switch} from "react-router-dom";
+import '../css/App.css';
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./homePage"
-import SignUp from "./SignUp";
-import SignIn from "./SignIn";
-import ToDoList from "./showTasks";
-import AddTasks from "./addTaskForm";
+import SignUp from "./user_Pages/SignUp";
+import SignIn from "./user_Pages/SignIn";
+import ToDoList from "./task_Pages/showTasks";
+import AddTasks from "./task_Pages/addTaskForm";
 function Home() {
     return (
         <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/LogIn" component={SignIn} />
-        <Route path="/Register" component={SignUp} />
-        <Route path="/ToDoList" component={ToDoList}/>
-        <Route path="/AddTasks" component={AddTasks} />
+            <Route exact path="/" component={HomePage} />
+            <Route path="/LogIn" component={SignIn} />
+            <Route path="/Register" component={SignUp} />
+            <Route path="/ToDoList" component={ToDoList} />
+            <Route path="/AddTasks" component={AddTasks} />
         </Switch>
     );
 }
