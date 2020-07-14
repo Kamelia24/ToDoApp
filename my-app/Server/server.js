a641d7ad455cf2647ddc7f70a2cc424757ad582d
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-14 13:43:36 +0300

    Update server.js

--- my-app/Server/server.js
+++ my-app/Server/server.js
@@ -6,5 +6,5 @@ app.use(cors());
 app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 app.use(Router);
-let port=5000;
-app.listen(port,()=>{console.log(`Node JS API is listening on port: ${port}`);});
+let port = 5000;
+app.listen(port, () => { console.log(`Node JS API is listening on port: ${port}`); });
