4ae38950d98e9e21d6d902af16165eeee62a7155
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-15 14:34:25 +0300

    Update showTasks.js

--- my-app/UI/src/task_Pages/showTasks.js
+++ my-app/UI/src/task_Pages/showTasks.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Task from "./tasks";
 import { Link, useHistory } from "react-router-dom";
+import M from 'materialize-css';
+import axios from 'axios';
 function ShowTask() {
     const history = useHistory();
+    let number;
+    let [isLoading, setIsLoading] = useState(true);
+    let [tasks, setTasks] = useState([]);
+    let [pagination, setPagination] = useState([<button className="active" key={1} onClick={() => movePage(1)}>{1}</button>]);
+    /* const movePage = (i) => {
+         axios.post('http://localhost:5000/getTasks', { num: i }, {
+             headers: {
+                 'Authorization': "Bearer " + localStorage.getItem("jwt")
+             }
+         })
+             .then((res) => {
+                 console.log("res", res.data)
+             })
+             .catch((error) => {
+                 console.log("errorrrr:", error)
+             });
+     }*/
+
+   
+    useEffect(() => {
+        axios.get('http://localhost:5000/getNumberOfTasks', {
+            headers: {
+                'Authorization': "Bearer " + localStorage.getItem("jwt")
+            }
+        })
+            .then((res) => {
+                console.log("res", res.data.num)
+                let pag = [];
+                number = res.data.num;
+                for (let i = 1; i <= number / 10 + 1; i++) {
+                    pag.push(<button key={i} onClick={() => movePage(i)}>{i}</button>)
+                }
+                setPagination(pag)
+            })
+            .catch((error) => {
+                console.log("errorrrr:", error)
+            });
+    }, [])
+
+ function movePage(i) {
+        if (i == undefined) { i = 1 }
+        axios.post('http://localhost:5000/getTasks', { num: i }, {
+            headers: {
+                'Authorization': "Bearer " + localStorage.getItem("jwt")
+            }
+        })
+            .then((res) => {
+                console.log("res", res)
+                setIsLoading(false);
+                setTasks(res.data["info"]);
+                console.log(tasks, isLoading)
+            })
+            .catch((error) => {
+                console.log("errorrrr:", error)
+                M.toast({ html: "I think you haven't logged in" })
+                history.push("/");
+                setIsLoading(false);
+                console.log(isLoading);
+            });
+    }
+    
     return (
         <div id="taskContainer">
             <div>
@@ -26,9 +89,19 @@ function ShowTask() {
                     </tr>
                 </thead>
                 <tbody>
-                    <Task />
+                    {!isLoading ? (
+                        <Task tasks={tasks} />
+                    ) : (
+                            <tr key={1}>
+
+                                <td >Loading...</td>
+                            </tr>
+                        )}
                 </tbody>
             </table>
+            <div className="topnav">
+                {pagination.map((pagination) => pagination)}
+            </div>
         </div>
     )
 }
