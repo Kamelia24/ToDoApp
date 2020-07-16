c3e0350c4401774a5a8ba5e829f5fdc288bc47e8
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-16 13:28:35 +0300

    Update tasks.js

--- my-app/UI/src/task_Pages/tasks.js
+++ my-app/UI/src/task_Pages/tasks.js
@@ -1,33 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import M from 'materialize-css';
-import { useHistory } from "react-router-dom";
 const Task = (tasks) => {
-    console.log(tasks)
-    const history = useHistory();
-    /*let [isLoading, setIsLoading] = useState(true);
-    let [tasks, setTasks] = useState([]);
-    useEffect(() => {
-        console.log(localStorage.getItem("jwt"))
-        axios.post('http://localhost:5000/getTasks',{num:i} ,{
-            headers: {
-                'Authorization': "Bearer " + localStorage.getItem("jwt")
-            }
-        })
-            .then((res) => {
-                console.log("res", res)
-                setIsLoading(false);
-                setTasks(res.data["info"]);
-                console.log(tasks, isLoading)
-            })
-            .catch((error) => {
-                console.log("errorrrr:", error)
-                M.toast({ html: "I think you haven't logged in" })
-                history.push("/");
-                setIsLoading(false);
-                console.log(isLoading);
-            });
-    }, [])*/
+    console.log(tasks.tasks)
     function removeTask(task) {
         console.log(task)
         axios.post('http://localhost:5000/removeTask', { id: task.taskID }, {
@@ -45,30 +20,24 @@ const Task = (tasks) => {
             })
     }
     return (
+        tasks["tasks"].map((task) => <tr key={task.id + 2}>
+
+            <td >{task.title}</td>
+
+            <td  >{task.description}</td>
+
+            <td  >{task.date_created}</td>
+            {task.status === "coming" ? (
+                <td style={{ color: 'red' }}>{task.deadline}</td>
+            ) : (
+                    <td  >{task.deadline}</td>
+                )}
 
 
-        
-            tasks.map((task) => <tr key={task.id + 2}>
+            <td ><input type="checkbox" className="checkmark" onClick={() => removeTask(task)}></input></td>
 
-                <td >{task.title}</td>
-
-                <td  >{task.description}</td>
-
-                <td  >{task.date_created}</td>
-                {task.status === "coming" ? (
-                    <td style={{ color: 'red' }}>{task.deadline}</td>
-                ) : (
-                        <td  >{task.deadline}</td>
-                    )}
-
-
-                <td ><input type="checkbox" className="checkmark" onClick={() => removeTask(task)}></input></td>
-
-            </tr>
-            
-            )
-        
-
+        </tr>
+        )
     )
 
 }
