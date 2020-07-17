65002b067c34ef5cb1691b5c659f584827523c37
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-17 13:34:04 +0300

    Update router.js

--- my-app/Server/router.js
+++ my-app/Server/router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controllers/controller.js');
 const userController = require('./controllers/userController.js');
-const requireLogin = require('./middleware/requireLogin')
+const requireLogin = require('./middleware/requireLogin');
+const requireAdmin = require('./middleware/requireAdmin');
 router.post('/checkUser', userController.checkUser);
 router.post('/addUser', userController.addUser);
 router.post('/addTask', requireLogin, controller.addTask);
 router.post('/getTasks', requireLogin, controller.getTasks);
 router.get('/getNumberOfTasks', requireLogin, controller.getNumOfTasks);
-router.post('/removeTask', requireLogin, controller.removeTask)
+router.post('/removeTask', requireLogin, controller.removeTask);
+router.get('/getUsers', requireLogin, requireAdmin, controller.getUsers)
 module.exports = router;
