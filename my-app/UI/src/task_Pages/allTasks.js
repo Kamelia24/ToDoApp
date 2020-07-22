ac913a2b82a69b03c46f58f254c3734e487f5e3b
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-22 15:22:51 +0300

    Update allTasks.js

--- my-app/UI/src/task_Pages/allTasks.js
+++ my-app/UI/src/task_Pages/allTasks.js
@@ -3,40 +3,15 @@ import Task from "./tasks";
 import { Link, useHistory } from "react-router-dom";
 import M from 'materialize-css';
 import axios from 'axios';
-function ShowUserTasks() {
+function ShowUserTasks(id) {
+    let userID = id.userID;
     const history = useHistory();
     let number;
     let [isLoading, setIsLoading] = useState(true);
     let [tasks, setTasks] = useState([]);
     let [pagination, setPagination] = useState([]);
-    let [users, setUsers] = useState([])
-    let [userID, setUserID] = useState(1)
     useEffect(() => {
-        axios.get('http://localhost:5000/getUsers', {
-            headers: {
-                'Authorization': "Bearer " + localStorage.getItem("jwt")
-            }
-        })
-            .then((res) => {
-                console.log("res", res.data.data)
-                let user = [];
-                //user = res.data.num;
-                for (let i = 1; i <= res.data.data.length; i++) {
-                    user.push(<button key={res.data.data[i - 1].id} onClick={() => setUserID(res.data.data[i - 1].id), () => getNumOfPages(res.data.data[i - 1].id)}>{res.data.data[i - 1]["username"]}</button>)
-                }
-                setUsers(user)
-                getNumOfPages(userID)
-                //getUserTasks(userID)
-
-            })
-            .catch((error) => {
-                console.log("errorrrr:", error)
-                M.toast({ html: "I think you haven't logged in" })
-                history.push("/");
-            });
-
-    }, [])
-    function getNumOfPages(userID) {
+        console.log(userID);
         axios.post('http://localhost:5000/getNumberOfTasks', { userID }, {
             headers: {
                 'Authorization': "Bearer " + localStorage.getItem("jwt")
@@ -58,7 +33,7 @@ function ShowUserTasks() {
                 M.toast({ html: "I think you haven't logged in" })
                 history.push("/");
             });
-    }
+    }, [])
     function getUserTasks(userID, i) {
         if (i === undefined) { i = 1 }
         axios.post('http://localhost:5000/getTasks', { num: i - 1, userID }, {
@@ -82,19 +57,7 @@ function ShowUserTasks() {
     }
     return (
         <div id="taskContainer">
-            <div>
-                <button className="button1" ><Link to="/ToDoList">My tasks</Link></button>
-                <button className="button1"
-                    onClick={() => {
-                        localStorage.clear()
-                        history.push("/")
-                    }}
-                >Log out</button>
-            </div>
-            <div className="topnav">
-                {users.map((users) => users)}
-            </div>
-            <h1 align="center">Your active tasks</h1>
+            <h1 align="center">Active tasks</h1>
             <table id="todo">
                 <thead>
                     <tr key={0}>
