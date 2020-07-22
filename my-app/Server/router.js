8f000b0c13e5b244ac4035c40a54425290c9a41d
Kamelia24 <62241242+Kamelia24@users.noreply.github.com>
2020-07-22 15:22:21 +0300

    Update router.js

--- my-app/Server/router.js
+++ my-app/Server/router.js
@@ -10,5 +10,6 @@ router.post('/addTask', requireLogin, controller.addTask);
 router.post('/getTasks', requireLogin, controller.getTasks);
 router.post('/getNumberOfTasks', requireLogin, controller.getNumOfTasks);
 router.post('/removeTask', requireLogin, controller.removeTask);
-router.get('/getUsers', requireLogin, requireAdmin, controller.getUsers)
+router.post('/getUsers', requireLogin, requireAdmin, controller.getUsers);
+router.get('/getNumberOfUsers',requireLogin,requireAdmin,controller.getNumOfUsers)
 module.exports = router;
